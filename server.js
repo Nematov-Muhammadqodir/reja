ffa86017841fdd92ce078366f29613f19ce202b6
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-02-21 16:30:15 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
 const app = express();
-
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Kirish kodlari
 app.use(express.static("public"));
@@ -19,9 +27,15 @@ app.post("/create-item", function (req, res) {
   console.log(req.body);
   res.json({ test: "Test success" });
 });
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
 app.get("/", function (req, res) {
   res.render("harid");
 });
+
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
