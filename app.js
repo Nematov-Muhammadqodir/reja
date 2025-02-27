478f4981568292fd397e4318a51fe16c256e3c1d
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-02-27 15:39:18 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+//MongoDB call
+const db = require("./server").db();
+
+let user;
+fs.readFile("database/user.json", "utf8", function (err, data) {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1 Kirish kodlari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2 Sessionga bogliq kodlar
+
+// 3 Viewsga doir codlar
+app.set("views", "views");
+app.set("view engine", "ejs");
+//4 Routing code
+app.post("/create-item", function (req, res) {
+  console.log(req.body);
+  res.json({ test: "Test success" });
+});
+
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,45 +1,29 @@
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", function (err, data) {
-  if (err) {
-    console.log("ERROR", err);
-  } else {
-    user = JSON.parse(data);
+let db;
+const connectionString =
+  "mongodb+srv://Kevin:Kevin_0301@cluster0.kifry.mongodb.net/Reja";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection to MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-// 1 Kirish kodlari
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2 Sessionga bogliq kodlar
-
-// 3 Viewsga doir codlar
-app.set("views", "views");
-app.set("view engine", "ejs");
-//4 Routing code
-app.post("/create-item", function (req, res) {
-  console.log(req.body);
-  res.json({ test: "Test success" });
-});
-
-app.get("/author", function (req, res) {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
